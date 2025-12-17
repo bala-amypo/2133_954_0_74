58416f3dae0a629780cafef9477ef574c082112f
bala-amypo <bala@amypo.in>
2025-12-17 10:32:20 +0000

    User commit

--- src/main/java/com/example/demo/service/Studentservice.java
+++ src/main/java/com/example/demo/service/Studentservice.java
@@ -1,28 +1,18 @@
-service:
+StudentService.java
 
-package com.example.practice.service;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.stereotype.Service;
-import com.example.practice.entity.StudentEntity;
-@Service
-public class Studentservice {
-    Map<Integer, StudentEntity> details = new HashMap<>();
+package com.example.demo.service;
 
-    public StudentEntity saveData(StudentEntity student) {
-        details.put(student.getId(), student);
-        return student;
-    }
-    public java.util.List<StudentEntity> getAllStudentEntity() {
-        return new java.util.ArrayList<>(details.values());
-    }
-     public void deleteStudentById(int id) {
-        details.remove(id);
-    }
-       public java.util.Optional<StudentEntity> getStudentById(int id) {
-        return java.util.Optional.ofNullable(details.get(id));
-    }
-    public void insertStudent(StudentEntity student) {
-        details.put(student.getId(), student);
-    }
+import java.util.List;
+import java.util.Optional;
+import com.example.demo.entity.Student;
+
+public interface StudentService {
+
+    Student insertStudent(Student st);
+
+    List<Student> getAllStudents();
+
+    Optional<Student> getOneStudent(Long id);
+
+    void deleteStudent(Long id);
 }

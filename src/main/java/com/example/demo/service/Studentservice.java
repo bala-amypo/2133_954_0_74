4b0de89dfd9637973f252e7052e961cd56b620c9
bala-amypo <bala@amypo.in>
2025-12-17 08:12:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Studentservice.java
@@ -0,0 +1,28 @@
+service:
+
+package com.example.practice.service;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+import com.example.practice.entity.StudentEntity;
+@Service
+public class Studentservice {
+    Map<Integer, StudentEntity> details = new HashMap<>();
+
+    public StudentEntity saveData(StudentEntity student) {
+        details.put(student.getId(), student);
+        return student;
+    }
+    public java.util.List<StudentEntity> getAllStudentEntity() {
+        return new java.util.ArrayList<>(details.values());
+    }
+     public void deleteStudentById(int id) {
+        details.remove(id);
+    }
+       public java.util.Optional<StudentEntity> getStudentById(int id) {
+        return java.util.Optional.ofNullable(details.get(id));
+    }
+    public void insertStudent(StudentEntity student) {
+        details.put(student.getId(), student);
+    }
+}
